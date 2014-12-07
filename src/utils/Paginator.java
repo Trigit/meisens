9dd5326cca34b9ab9e32b492a92195b212fb9a54
Trigit <trigit@163.com>
2014-12-07 17:07:26 +0800

    使用注解

---
+++ src/utils/Paginator.java
@@ -0,0 +1,73 @@
+package utils;
+
+/**
+ * @Class Description:分页组件
+ * @author Boki
+ * @Date 2014年12月7日 - 下午4:43:16
+ */
+public class Paginator {
+	private int totalPage;
+	private int previousPage;
+	private int nextPage;
+	private int pageSize;
+	private int firstPage;
+	private int lastPage;
+	private int currentPage;
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public int getTotalPage() {
+		return totalPage;
+	}
+
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+
+	public int getPreviousPage() {
+		return previousPage;
+	}
+
+	public void setPreviousPage(int previousPage) {
+		this.previousPage = previousPage;
+	}
+
+	public int getNextPage() {
+		return nextPage;
+	}
+
+	public void setNextPage(int nextPage) {
+		this.nextPage = nextPage;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getFirstPage() {
+		return firstPage;
+	}
+
+	public void setFirstPage(int firstPage) {
+		this.firstPage = firstPage;
+	}
+
+	public int getLastPage() {
+		return lastPage;
+	}
+
+	public void setLastPage(int lastPage) {
+		this.lastPage = lastPage;
+	}
+
+}
