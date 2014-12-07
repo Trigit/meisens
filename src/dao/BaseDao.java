7e6e931d141fe70d143ba45d66c793148cdc2161
Trigit <trigit@163.com>
2014-12-07 21:27:35 +0800

    BaseDao增加按条件查询的方法

--- src/dao/BaseDao.java
+++ src/dao/BaseDao.java
@@ -1,6 +1,9 @@
 package dao;
 
 import java.io.Serializable;
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -13,7 +16,7 @@ import org.hibernate.classic.Session;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
-import utils.BeanAccessUtils;
+import pojo.QueryCondition;
 
 /**
  * @Class Description:dao基类
@@ -46,6 +49,13 @@ public class BaseDao {
 		return (T) getSession().get(clazz, id);
 	}
 
+	/**
+	 * 等值查询
+	 * 
+	 * @param clazz
+	 * @param conditionMap
+	 * @return
+	 */
 	public List getByCondition(Class<?> clazz, Map<String, Object> conditionMap) {
 		StringBuilder sbuilder = new StringBuilder("from ");
 		sbuilder.append(clazz.getSimpleName());
@@ -65,5 +75,121 @@ public class BaseDao {
 			query.setParameter(entry.getKey(), entry.getValue());
 		}
 		return query.list();
+	}
+
+	/**
+	 * 按条件查询
+	 * 
+	 * @param clazz
+	 * @param conditionMap
+	 * @return
+	 */
+	public List getByQueryCondition(Class<?> clazz, Map<String, QueryCondition> conditionMap) {
+		StringBuilder sbuilder = new StringBuilder("from ");
+		sbuilder.append(clazz.getSimpleName());
+		sbuilder.append(" t where 1 = 1");
+
+		for (Entry<String, QueryCondition> entry : conditionMap.entrySet()) {
+			String key = entry.getKey();
+			QueryCondition condition = entry.getValue();
+			sbuilder.append(" and t.");
+			sbuilder.append(key);
+			sbuilder.append(" ");
+
+			switch (condition.getConditioType()) {
+			case EQUALS:
+				sbuilder.append("=?");
+				break;
+			case LIKE:
+				sbuilder.append(" like ?");
+				break;
+			case IN:
+				sbuilder.append("in(");
+				sbuilder.append(combindParams(condition.getValue()));
+				sbuilder.append(")");
+				break;
+			case BETWEEN:
+				sbuilder.append("between(?,?)");
+				break;
+			case GREATERTHAN:
+				sbuilder.append(">?");
+				break;
+			case LESSTHAN:
+				sbuilder.append("<?");
+				break;
+			case NOTGREATERTHAN:
+				sbuilder.append("<=?");
+				break;
+			case NOTLESSTHAN:
+				sbuilder.append(">=?");
+				break;
+			case NOTIN:
+				sbuilder.append("not in(");
+				sbuilder.append(combindParams(condition.getValue()));
+				sbuilder.append(")");
+				break;
+			}
+		}
+
+		Query query = getSession().createQuery(sbuilder.toString());
+
+		int paramIndex = 0;
+		for (QueryCondition condition : conditionMap.values()) {
+			switch (condition.getConditioType()) {
+			case EQUALS:
+			case GREATERTHAN:
+			case LESSTHAN:
+			case NOTGREATERTHAN:
+			case NOTLESSTHAN:
+				query.setParameter(paramIndex++, condition.getValue());
+				break;
+			case LIKE:
+				query.setParameter(paramIndex++, "%" + condition.getValue() + "%");
+				break;
+			case BETWEEN:
+			case IN:
+			case NOTIN:
+				Collection<?> collection = null;
+				if (condition.getValue().getClass().isArray()) {
+					collection = Arrays.asList((Object[]) condition.getValue());
+				} else if (condition.getValue() instanceof Collection<?>) {
+					collection = (Collection<?>) condition.getValue();
+				}
+				if (collection == null) {
+					break;
+				}
+				for (Object item : collection) {
+					query.setParameter(paramIndex++, item);
+				}
+				break;
+			}
+		}
+
+		return query.list();
+	}
+
+	/**
+	 * 条件转为"?,"连接的形式
+	 * 
+	 * @param values
+	 * @return
+	 */
+	private String combindParams(Object values) {
+		int size = 0;
+		if (values.getClass().isArray()) {
+			size = ((Object[]) values).length;
+		} else if (values instanceof Collection<?>) {
+			size = ((Collection<?>) values).size();
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < size; i++) {
+			sb.append("?,");
+		}
+		if (sb.length() > 0) {
+			sb.deleteCharAt(sb.length() - 1);
+		}
+
+		return sb.toString();
 	}
 }
